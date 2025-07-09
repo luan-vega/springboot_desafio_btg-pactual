26a3a4c1ae30cafaf8a775c7934932ba8282e3cd
luan-vega <luandlavega@gmail.com>
2025-07-09 12:15:43 -0300

    feat: ajuste log

--- src/main/java/com_springboot_/btg_pactual/controller/OrderController.java
+++ src/main/java/com_springboot_/btg_pactual/controller/OrderController.java
@@ -4,17 +4,17 @@ import com_springboot_.btg_pactual.controller.dto.ApiResponse;
 import com_springboot_.btg_pactual.controller.dto.OrderResponse;
 import com_springboot_.btg_pactual.controller.dto.PaginationResponse;
 import com_springboot_.btg_pactual.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-
+// C:/Users/luand/OneDrive/Documentos/Estudos/TI/Desafios/springboot_desafio_btg-pactual/src/main/java/com_springboot_/btg_pactual/controller/OrderController.java
+@Slf4j
 @RestController
 public class OrderController {
-
-    //private final Logger logger = LoggerFactory.getLogger();
 
     private OrderService orderService;
 
@@ -26,15 +26,20 @@ public class OrderController {
     public ResponseEntity<ApiResponse<OrderResponse>> listOrders(@PathVariable("customerId") Long customerId,
                                                                  @RequestParam(name = "page", defaultValue = "0") Integer page,
                                                                  @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+        // 1. Busca os pedidos de forma paginada
         var pageResponse = orderService.findAllByCustumerId(customerId, PageRequest.of(page, pageSize));
+
+        // 2. Busca o valor total de todos os pedidos do cliente
         var totalOnOrders = orderService.findTotalOnOrdersByCustomerId(customerId);
 
+        // 3. Monta e retorna a resposta final
         return ResponseEntity.ok(new ApiResponse<>(
                 Map.of("totalOnOrders", totalOnOrders),
                 pageResponse.getContent(),
                 PaginationResponse.fromPage(pageResponse)
         ));
-        //logger.info("Page response: {}", pageResponse);
+        
+       // log.info("Page response: {}", pageResponse);
     }
-
 }
+
